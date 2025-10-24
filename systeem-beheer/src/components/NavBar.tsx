5844ee95d82d3240afe58ab92a6bcf229a2347c3
odinVos <1197607@student.roc-nijmegen.nl>
2025-10-24 16:11:26 +0200

    Added styling

---
+++ systeem-beheer/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="navtop">
+        <p>-</p>
+      </div>
+      <div className="navbottom">
+        <p>-</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
--- systeem-beheer/src/components/ScanPasLender.tsx
+++ systeem-beheer/src/components/ScanPasLender.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Lender from "../models/Lender";
+import NavBar from "./NavBar";
 
-function ScanPasLender(props:{setLender: (lender: Lender) => void}) {
-
+function ScanPasLender(props: { setLender: (lender: Lender) => void }) {
   function getLenderByPassNumber(passNumber: string): Lender | undefined {
     if (passNumber === "12345") {
       return {
         id: "1",
         name: "John Doe",
-        passNumber: "12345"
+        passNumber: "12345",
       };
     }
     return undefined;
@@ -16,24 +16,40 @@ function ScanPasLender(props:{setLender: (lender: Lender) => void}) {
 
   function handleSubmit(formData: FormData) {
     const passNumber: string = formData.get("pasnummer") as string;
-    var lender: Lender | undefined = getLenderByPassNumber(passNumber as string);
+    var lender: Lender | undefined = getLenderByPassNumber(
+      passNumber as string
+    );
     if (lender == undefined) {
       lender = {
         id: "",
         name: "",
-        passNumber: passNumber
+        passNumber: passNumber,
       };
     }
     props.setLender(lender);
   }
 
   return (
-    <form action={handleSubmit}>
-      ScanPasLender:
-      <input type="text" name="pasnummer" />
-      <button type="submit">Volgende</button>
-      <button type="button">Annuleren</button>
-    </form>
+    <>
+      <NavBar />
+      <div className="card">
+        <p className="card-title">Scan uw schoolpas</p>
+        <form action={handleSubmit}>
+          <div className="test">
+            Voer hier het pasID in
+            <input type="text" name="pasnummer" />
+          </div>
+          <div className="card-button">
+            <button type="button" className="vorige">
+              Annuleren
+            </button>
+            <button type="submit" className="volgende">
+              Volgende
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
