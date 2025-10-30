053b15e37941c648055f35a9abd6d7055170ede1
Karsten <karstenseijkens@gmail.com>
2025-10-30 15:10:03 +0100

    WIP autocomplete

--- systeem-beheer/src/components/autoComplete/AutoComplete.tsx
+++ systeem-beheer/src/components/autoComplete/AutoComplete.tsx
@@ -3,6 +3,7 @@ import "./AutoComplete.css";
 
 function AutoComplete<T>(props: {
   onSearch: (searchParam: string, items: T[]) => T[] | Promise<T[]>,
+  getLabel?: (item: T) => string,
   items: T[],
   labelKey?: string,
   inputStyle?: React.CSSProperties,
@@ -10,24 +11,37 @@ function AutoComplete<T>(props: {
 }) {
   const [results, setResults] = React.useState<T[]>([]);
   const [value, setValue] = React.useState<T | undefined>();
+  const [inputText, setInputText] = React.useState<string>("");
 
   async function HandleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const searchParam: string = event.target.value;
+    setInputText(searchParam);
+    if (value && GetLabel(value) != searchParam) {
+      setValue(undefined);
+    }
     setResults(await props.onSearch(searchParam, props.items));
   }
 
-  function SetValueWithInde(event: React.MouseEvent<HTMLButtonElement>) {
-    if (event.currentTarget.getAttribute("data-index") == null) {
-      console.warn("data-index is null");
-      return;
+  function HandleBlur(event: React.FocusEvent<HTMLInputElement>) {
+    if (!value || GetLabel(value) != inputText) {
+      setValue(undefined);
+      setInputText("");
     }
-    const indexStr: string = event.currentTarget.getAttribute("data-index") ?? "";
-    const index: number = parseInt(indexStr);
+    setResults([]);
+  }
+
+  function SetValueWithIndex(event: React.MouseEvent<HTMLButtonElement>, index: number) {
+    console.log("hit set value");
+    event.preventDefault();
     const selectedItem = results[index];
     setValue(selectedItem);
+    setInputText(GetLabel(selectedItem));
   }
 
   function GetLabel(item: T): string {
+    if (props.getLabel) {
+      return props.getLabel(item);
+    }
     if (props.labelKey) {
       const keys = props.labelKey.split(".");
       var value: any = item;
@@ -41,13 +55,13 @@ function AutoComplete<T>(props: {
 
   function DropdownItems() {
     return results.map((result: T, index: number) => (
-      <button onClick={SetValueWithInde} key={index}>{GetLabel(result)}</button>
+      <button className="dropdown-item" onClick={(event) => SetValueWithIndex(event, index)} key={index}>{GetLabel(result)}</button>
     ));
   }
 
   return (
     <div style={props.style}>
-      <input type="text" onChange={HandleChange} style={props.inputStyle}/>
+      <input type="text" value={inputText} onChange={HandleChange} onBlur={HandleBlur} style={props.inputStyle}/>
       {results.length > 0 ? <div className="dropdown">
         {DropdownItems()}
       </div> : null}
