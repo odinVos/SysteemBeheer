5349343365175f8418f6b04ecf575f08a045123f
odinVos <1197607@student.roc-nijmegen.nl>
2025-11-28 14:18:27 +0100

    Navbar toegevoegd
    Er moet nog geken worden naar de routing, de tekst hoogte en locatie van de image

--- systeem-beheer/src/components/NavBar.tsx
+++ systeem-beheer/src/components/NavBar.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const [activePage, setActivePage] = useState<string>("scan-pas");
+
+  function goToPage() {
+    navigate("scan-pas");
+  }
+
   return (
     <div className="navbar">
       <div className="navtop">
         <p>-</p>
       </div>
       <div className="navbottom">
-        <p>-</p>
+        <ul>
+          <li>
+            <img id="logo" src={require("./logo-roc-nijmegen.png")} />
+          </li>
+          <li onClick={goToPage}>Home</li>
+          <li>Uitlening afhandelen</li>
+          <li>Product uitlenen</li>
+          <li>Uitlening registreren</li>
+          <li>Product toevoegen</li>
+          <li>Product verwijderen</li>
+        </ul>
       </div>
     </div>
   );
